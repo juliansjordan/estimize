3c3f2248c2b28cf0ace28d241cd67222e70de154
juliansjordan <82381487+juliansjordan@users.noreply.github.com>
2021-04-14 21:28:54 -0400

    Update captsone project.py

--- captsone project.py
+++ captsone project.py
@@ -31,38 +31,53 @@ estimates.describe()
 #Consensus: ticker, date, estimize stats (weighted avg. high, low, SD, count, for EPS, revenue), actuals
 #Estimates (granular): EPS, revenue, ticker, fiscal year, fiscal quarter, reported EPS, reported revenue, user bio, etc.
 
-#%%Clean up data
+#%%Clean up data and add new data elements
 # Update data types
 # Add binary flag to EPS / Revenue
-# Diff between reporting date 
 
-consensus.dtypes
-
-#Make a smaller file
-consensus_small = consensus.head(1000)
-consensus_small['reports_at'] = pd.to_datetime(consensus_small['reports_at'])
-consensus_small['reports_at'] = consensus_small[['reports_at']].to_string()
-
-consensus_small.dtypes
-
-#Convert to substring, then cast as date --- got stuck here.
-consensus_small['reports_at_substr'] = consensus_small['reports_at'].str[:9]
- 
-
-
-#Cast data types to date
-consensus['date'] = pd.to_datetime(consensus['date'])
-consensus['reports_at'] = pd.to_datetime(consensus['reports_at'], utc = True)
-
-#Has wall street reached consensus?
-#consensus['ws_flag'] = pd.to_datetime(consensus['date'])
 consensus['ws_flag'] = consensus['wallstreet.eps'].notnull()
-    #For others: the flag is a True, False (not 1,0) --> remember that it's case sensitive
-    
-#Difference in dates
-consensus['datediff'] = pd. consensus['reports_at'] - consensus['date']
 
-#%%Add new data elements
+# Diff between reporting date 
+consensus['reports_at'] = pd.to_datetime(consensus['reports_at'], utc = True)
+consensus['date'] = pd.to_datetime(consensus['date'], utc = True)
+consensus['datediff'] = consensus['reports_at'] - consensus['date'] 
+
+consensus['datediff_in_days'] = consensus['datediff'].dt.days 
+
+#Difference between values in estimize vs. wallstreet
+#EPS
+consensus['eps_diff_abs'] = consensus['estimize.eps.weighted'] - consensus['actual.eps']
+consensus['eps_diff_percent'] = (consensus['estimize.eps.weighted'] - consensus['actual.eps'])/consensus['actual.eps']
+consensus['eps_diff_percent_abs'] = consensus['eps_diff_percent'].abs
+
+#Revenue
+consensus['rev_diff_abs'] = consensus['estimize.revenue.weighted'] - consensus['actual.revenue']
+consensus['rev_diff_percent'] = (consensus['estimize.revenue.weighted'] - consensus['actual.revenue'])/consensus['actual.revenue']
+consensus['rev_diff_percent_abs'] = consensus['eps_diff_percent'].abs
+
 
 
 #%%Exploratory data analysis
+#Number of unique tickers
+    consensus.groupby(['ticker'])['ticker'].nunique()
+
+#Min date
+    consensus['date'].min() ##--> 2010-09-29
+    consensus['date'].max() ##--> 2020-12-30
+
+#Get some graphics going
+  #Look at ranges in lags in diff between report dates & 
+     consensus.boxplot(column=['datediff_in_days']) #--> seems like there is a large range
+     plt.boxplot(x=consensus['datediff_in_days']) #--> just trying out another metho dof plotting
+  
+  #Look at differences in EPS, revenue  
+      consensus.boxplot(column=['eps_diff_percent', 'rev_diff_percent']) #--> really large differences... need to figure out why the differences are so high
+
+      consensus.boxplot(column=['eps_diff_abs', 'rev_diff_abs']) #--> really large differences... need to figure out why the differences are so high
+
+    consensus.boxplot(column=['eps_diff_abs']) #--> we have some bad data; really extreme values that are outliers
+
+#%%Analysis
+    #Create a conditional column that has the standard error based on whether the ws_flag is true or flase
+    #Then we can run a t-test to see if the differences in error values are stastically significant
+    #First need to clean up data though - there ar esome large outliers on EPS, Revenue

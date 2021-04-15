aaad885d3d895cd438518ce2d9ce027e7d270bfc
gwelds <82392446+gwelds@users.noreply.github.com>
2021-04-15 13:57:34 -0400

    Update captsone project.py

--- captsone project.py
+++ captsone project.py
@@ -77,7 +77,46 @@ consensus['rev_diff_percent_abs'] = consensus['eps_diff_percent'].abs
 
     consensus.boxplot(column=['eps_diff_abs']) #--> we have some bad data; really extreme values that are outliers
 
-#%%Analysis
-    #Create a conditional column that has the standard error based on whether the ws_flag is true or flase
-    #Then we can run a t-test to see if the differences in error values are stastically significant
-    #First need to clean up data though - there ar esome large outliers on EPS, Revenue
+#%% Addressing Outliers
+
+consensus = consensus[consensus['datediff_in_days'] < 500] #removes long-term estimates
+consensus = consensus[consensus['eps_diff_percent_abs'] < 5] #removes some outliers (what should be the right cutoffs)
+consensus = consensus[consensus['rev_diff_percent_abs'] < 5] # same as above
+
+
+#%% Extract List of Stocks of Interest (had no wall street est at some stage)
+
+cons_tickers = consensus[consensus['ws_flag'] != True]
+tickers = cons_tickers.groupby(['ticker'])['ticker'].nunique()
+tickers = tickers.index
+tickers = tickers.to_series()
+
+#%% Extract rows with these tickers from the original dataset into new dataset
+
+new_cons = consensus[consensus['ticker'].isin(tickers)]
+
+#%% Initial Analysis to see if erros vary between our ws flag
+
+new_cons.groupby(['ws_flag'])['eps_diff_percent_abs'].mean() # shows a significant difference in average absolute error in expected direction
+new_cons.groupby(['ws_flag'])['rev_diff_percent_abs'].mean() # same as above
+
+#%% Run regression to test for size and significance of the dummy coefficient
+
+# First set up dummy variable for ws flag that can be interpreted by regression
+
+cons_dummy = pd.get_dummies(new_cons['ws_flag']) # create dummy variables for the wall street flag
+new_cons['ws_dummy'] = cons_dummy.iloc[:,1] #add dummy variables to the main dataset
+
+# Set up the regression
+eps_ols1 = smf.ols('eps_diff_percent_abs ~ C(ws_dummy) + datediff_in_days', data = new_cons).fit()
+eps_ols1.summary() #R2 is low (but not a big deal bc we aren't predicting), P-value is significant and coefficient suggests meaningful change
+
+eps_ols2 = smf.ols('rev_diff_percent_abs ~ C(ws_dummy) + datediff_in_days', data = new_cons).fit()
+eps_ols2.summary() #similar takeaways as above
+
+#%% Next Steps
+
+# The special tickers still includes stocks that definitely had WS coverage but the estimize dataset doesn't include consensus
+# We also don't have a clear time-series definied (i.e. starts off false and turns true) for a before and after analysis
+# Need to figure out how to modify dataset for the second scenario
+# We can then expand this analysis to look at trends in overestimates vs. underestimates

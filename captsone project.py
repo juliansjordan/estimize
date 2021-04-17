f42c7db7c40eec2102e2ca158edd7e77015bbdf6
gwelds <82392446+gwelds@users.noreply.github.com>
2021-04-17 16:49:23 -0400

    Update captsone project.py

--- captsone project.py
+++ captsone project.py
@@ -83,6 +83,7 @@ consensus = consensus[consensus['datediff_in_days'] < 500] #removes long-term es
 consensus = consensus[consensus['eps_diff_percent_abs'] < 5] #removes some outliers (what should be the right cutoffs)
 consensus = consensus[consensus['rev_diff_percent_abs'] < 5] # same as above
 
+
 #%% Extract List of Stocks of Interest (had no wall street est at some stage)
 
 cons_tickers = consensus[consensus['ws_flag'] != True]
@@ -115,13 +116,84 @@ new_cons['ws_dummy'] = cons_dummy.iloc[:,1] #add dummy variables to the main dat
 eps_ols1 = smf.ols('eps_diff_percent_abs ~ C(ws_dummy) + datediff_in_days', data = new_cons).fit()
 eps_ols1.summary() #R2 is low (but not a big deal bc we aren't predicting), P-value is significant and coefficient suggests meaningful change
 
-eps_ols2 = smf.ols('rev_diff_percent_abs ~ C(ws_dummy) + datediff_in_days', data = new_cons).fit()
-eps_ols2.summary() #similar takeaways as above
+rev_ols1 = smf.ols('rev_diff_percent_abs ~ C(ws_dummy) + datediff_in_days', data = new_cons).fit()
+rev_ols1.summary() #similar takeaways as above
 
+#%% Create Sector Segments to view Errors
+
+sector_errors = new_cons.groupby(['instrument_sector', 'ws_flag'])['eps_diff_percent_abs'].mean() # calculate average eps errors by industry
+sector_errors = sector_errors.to_frame() # convert to dataframe
+sector_errors = sector_errors.reset_index() 
+sector_errors = sector_errors.pivot(index = 'instrument_sector', columns = 'ws_flag') #create pivot table to compare errors by industry / flag
+sector_errors['error_diff'] = sector_errors['eps_diff_percent_abs',False] - sector_errors['eps_diff_percent_abs',True] # create new column for the difference in average errors
+
+# Make Charts to show dispersion of errors by sector
+
+
+#%% Identify ratio missing wallstreet cons to listed wall street cons per stock
+
+est_missing = []
+for i in tickers:
+    df = new_cons[new_cons['ticker'].isin([i])]
+    a = df['wallstreet.eps'].isna().sum() / df['wallstreet.eps'].count()
+    est_missing.append(a)
+
+est_missing = pd.DataFrame(est_missing, columns = ['est_missing'])
+tickers_list = tickers.tolist()
+est_missing['ticker'] = tickers_list
+
+#%% Create new tickers list with stocks missing >50% of wall street cons
+
+new_tickers = est_missing[est_missing['est_missing'] > 0.999]
+new_tickers = new_tickers.drop(['est_missing'], axis = 1)
+new_tickers = new_tickers.reset_index()
+new_tickers = new_tickers.drop(['index'], axis = 1)
+
+#%% Create database using filtered ticker list and re-run analysis
+
+small_cons = consensus[consensus['ticker'].isin(new_tickers['ticker'])]
+small_cons.groupby(['ws_flag'])['eps_diff_percent_abs'].mean() # shows a significant difference in average absolute error in unexpected direction
+small_cons.groupby(['ws_flag'])['rev_diff_percent_abs'].mean() # not a significant difference
+
+small_dummy = pd.get_dummies(small_cons['ws_flag']) # create dummy variables for the wall street flag
+small_cons['ws_dummy'] = small_dummy.iloc[:,1] #add dummy variables to the main dataset
+
+eps_ols2 = smf.ols('eps_diff_percent_abs ~ C(ws_dummy) + datediff_in_days', data = small_cons).fit()
+eps_ols2.summary() #R2 is low (but not a big deal bc we aren't predicting), P-value is significant and coefficient suggests meaningful change
+
+rev_ols2 = smf.ols('rev_diff_percent_abs ~ C(ws_dummy) + datediff_in_days', data = small_cons).fit()
+rev_ols2.summary() #similar takeaways as above
+
+#%%Sector analysis on filtered ticker list
+
+small_sector_errors = small_cons.groupby(['instrument_sector', 'ws_flag'])['eps_diff_percent_abs'].mean() # calculate average eps errors by industry
+small_sector_errors = small_sector_errors.to_frame() # convert to dataframe
+small_sector_errors = small_sector_errors.reset_index() 
+small_sector_errors = small_sector_errors.pivot(index = 'instrument_sector', columns = 'ws_flag') #create pivot table to compare errors by industry / flag
+small_sector_errors['error_diff'] = small_sector_errors['eps_diff_percent_abs',False] - small_sector_errors['eps_diff_percent_abs',True] # create new column for the difference in average errors
+
+#add some charts to show dispersion of errors by sector
+
+
+#%% Custom stock list - focused on small/microcap names in the dataset with minimal coverage
+
+final_stocks = ['XXII','BCPC','CALM','CKP','DLX','FDP','FRO','GIVN','GSC','HMTV','HOV','IOSP','ISCA','JBSS','KELY','LNCR','MHLD','MFB','MM','NVEC','OTTR','PDLI','PKE','SAFT','SWM','SPOK','TUEM','TOUR','VCLK','VMEM']
+final_cons = consensus[consensus['ticker'].isin(final_stocks)]
+
+#%% Repeat analysis from above
+
+final_cons.groupby(['ws_flag'])['eps_diff_percent_abs'].mean() # shows a significant difference in average absolute error in expected direction
+final_cons.groupby(['ws_flag'])['rev_diff_percent_abs'].mean() # 
+
+final_dummy = pd.get_dummies(final_cons['ws_flag']) # create dummy variables for the wall street flag
+final_cons['ws_dummy'] = final_dummy.iloc[:,1] #add dummy variables to the main dataset
+
+eps_ols3 = smf.ols('eps_diff_percent_abs ~ C(ws_dummy) + datediff_in_days', data = final_cons).fit() #ws coverage seems to improve estimize user accuracy
+eps_ols3.summary() 
+
+rev_ols3 = smf.ols('rev_diff_percent_abs ~ C(ws_dummy) + datediff_in_days', data = final_cons).fit()
+rev_ols3.summary() #same as above
 
 #%% Next Steps
-
-# The special tickers still includes stocks that definitely had WS coverage but the estimize dataset doesn't include consensus
-# We also don't have a clear time-series definied (i.e. starts off false and turns true) for a before and after analysis
-# Need to figure out how to modify dataset for the second scenario
-# We can then expand this analysis to look at trends in overestimates vs. underestimates
+# Add charts for visual representation of the analysis
+# Expand this analysis to look at trends in overestimates vs. underestimates
